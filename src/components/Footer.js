cf4107314c9f7ee281cc607d5742f6652340305d
Soegidev <soegidev.id@gmail.com>
2023-05-31 12:13:42 +0700

    init

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,17 +7,17 @@ const Footer = () => {
     <div className="flex justify-between bottom-0">
       <div className="flex border-hollandtints-700 ml-4 mr-4 mt-2 mb-2 justify-start">
         <img src={Instagramicon} alt="" className="h-10 w-10" />
-        <h3 className="text-xl font-medium mx-3 p-2">@hollandbakery</h3>
+        <h3 className="text-xl font-medium mx-3 p-2 -ml-0">@hollandbakery</h3>
       </div>
       <div className="flex border-hollandtints-700 ml-4 mr-4 mt-2 mb-2">
         <img src={FacebookIcon} alt="" className="h-10 w-10" />
-        <h3 className="text-xl font-medium mx-3 p-2">
+        <h3 className="text-xl font-medium mx-3 p-2 -ml-0">
           Holland Bakery Indonesia (Official)
         </h3>
       </div>
       <div className="flex border-hollandtints-700 ml-4 mr-4 mt-2 mb-2 justify-end">
         <img src={WebsiteIcon} alt="" className="h-10 w-10" />
-        <h3 className="text-xl font-medium mx-3 p-2">
+        <h3 className="text-xl font-medium mx-3 p-2 -ml-0">
           www.hollandbakery.co.id
         </h3>
       </div>
